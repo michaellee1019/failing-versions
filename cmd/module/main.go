0689dbabeaf14cd1b7961351764691449f52f966
Michael Lee <michaellee1019@gmail.com>
2025-12-19 13:48:43 -0500

    duh

--- cmd/module/main.go
+++ cmd/module/main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"failingversions"
+
+	sensor "go.viam.com/rdk/components/sensor"
 	"go.viam.com/rdk/module"
 	"go.viam.com/rdk/resource"
-	sensor "go.viam.com/rdk/components/sensor"
 )
 
 func main() {
-	// ModularMain can take multiple APIModel arguments, if your module implements multiple models.
-	module.ModularMain(resource.APIModel{ sensor.API, failingversions.TestModel})
+	panic("Im failing on version 0.0.3")
 
-	panic("Im failing on version 0.0.2")
+	// ModularMain can take multiple APIModel arguments, if your module implements multiple models.
+	module.ModularMain(resource.APIModel{sensor.API, failingversions.TestModel})
 }
